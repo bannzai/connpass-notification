879693d375454421a215da1deb458046e167e18e
bannzai <kingkong999yhirose@gmail.com>
2019-03-10 00:15:44 +0900

    :recycle: Using let

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-var Settings = settings();
+let Settings = settings();
 
 function main() {
-  var threads = GmailApp.search(Settings.searchWord);
-  var count = threads.length;
+  let threads = GmailApp.search(Settings.searchWord);
+  let count = threads.length;
 
-  for (var i = 0; i < count; i++) {
+  for (let i = 0; i < count; i++) {
     let thread = threads[i];
     //      if (!thread.isUnread()) {
     //      // While already read mail.
@@ -19,17 +19,17 @@ function main() {
 }
 
 function sendIfExpected(thread) {
-  var subject = thread.getFirstMessageSubject();
+  let subject = thread.getFirstMessageSubject();
   if (!expectedEvent(subject)) {
     console.log(Utilities.formatString("Unexpected mail subject, got %s", subject));
     return;
   }
-  var event = matchedEvent(subject);
-  var messageHTMLBody = thread.getMessages()[0].getBody();
-  var permanentLink = thread.getPermalink();
-  var timestamp = thread.getLastMessageDate().getTime() / 1000;
+  let event = matchedEvent(subject);
+  let messageHTMLBody = thread.getMessages()[0].getBody();
+  let permanentLink = thread.getPermalink();
+  let timestamp = thread.getLastMessageDate().getTime() / 1000;
 
-  var jsonPayload = {
+  let jsonPayload = {
     "fallback": "Notfify connpass events from gmail",
     "channel": event.channel,
     "username": Settings.appName,
@@ -43,17 +43,17 @@ function sendIfExpected(thread) {
       }
     ],
   };
-  var payload = JSON.stringify(jsonPayload);
-  var options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+  let payload = JSON.stringify(jsonPayload);
+  let options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
     "method": "post",
     "contentType": "application/json",
     "payload": payload,
     "muteHttpExceptions": true
   };
 
-  var response = UrlFetchApp.fetch(event.webhookURL, options);
-  var responseCode = response.getResponseCode();
-  var responseBody = response.getContentText();
+  let response = UrlFetchApp.fetch(event.webhookURL, options);
+  let responseCode = response.getResponseCode();
+  let responseBody = response.getContentText();
 
   if (responseCode != 200) {
     console.log(Utilities.formatString("Request failed. Expected 200, got %d: %s", responseCode, responseBody));
