10b691d39d75d10c6246af706d48c5c95622cd92
bannzai <kingkong999yhirose@gmail.com>
2019-03-09 23:58:07 +0900

    :recycle: Fix events

--- src/events.ts
+++ src/events.ts
@@ -1,46 +1,89 @@
-var EVENTS = createEnum({
-  REGISTER : {
-    value : 1,
-    suffix : 'に参加申し込みがありました',
-    color: "#00FF00",
-    emoticon: ":tada:",
-    webhookURL: Settings.registerWebHookURL,
-    channel: Settings.registerNotificationChannel
-  },
-  CANCEL : {
-    value : 2,
-    suffix : 'に参加キャンセルがありました',
-    color: "#FF0000",
-    emoticon: ":x:",
-    webhookURL: Settings.cancelWebHookURL,
-    channel: Settings.cancelNotificationChannel
-  },
-  CONTACT : {
-    value : 3,
-    suffix : 'に関するお問い合わせ',
-    color: "#0000FF",
-    emoticon: ":email:",
-    webhookURL: Settings.contactWebHookURL,
-    channel: Settings.contactNotificationChannel
+enum Events {
+  Register,
+  Cancel,
+  Contact
+}
+
+namespace Events {
+  export function suffix(event: Events): string {
+    switch (event) {
+      case Events.Register:
+        return 'に参加申し込みがありました';
+      case Events.Cancel:
+        return 'に参加キャンセルがありました';
+      case Events.Contact:
+        return 'に関するお問い合わせ';
+    }
   }
-});
+
+  export function color(event: Events): string {
+    switch (event) {
+      case Events.Register:
+        return '#00FF00';
+      case Events.Cancel:
+        return '#FF0000';
+      case Events.Contact:
+        return '#0000FF';
+    }
+  }
+
+  export function emoticon(event: Events): string {
+    switch (event) {
+      case Events.Register:
+        return ':tada:';
+      case Events.Cancel:
+        return ':x:';
+      case Events.Contact:
+        return ':email:';
+    }
+  }
+
+  export function webhookURL(event: Events): string {
+    switch (event) {
+      case Events.Register:
+        return Settings.registerWebHookURL;
+      case Events.Cancel:
+        return Settings.cancelWebHookURL;
+      case Events.Contact:
+        return Settings.contactWebHookURL;
+    }
+  }
+
+  export function channel(event: Events): string {
+    switch (event) {
+      case Events.Register:
+        return Settings.registerNotificationChannel;
+      case Events.Cancel:
+        return Settings.cancelNotificationChannel;
+      case Events.Contact:
+        return Settings.contactNotificationChannel;
+    }
+  }
+
+  export function all(): [Events] {
+    return Object.keys(Events).map(
+      k => Events[k]
+    ) as [Events];
+  }
+
+}
 
 function matchedEvent(subject) {
-  var all = EVENTS.all();
+  var all = Events.all();
   var e;
-  all.forEach(function(event) {
-    if (contains(subject, event.suffix)) {
-       e = event;
+  all.forEach(function (event) {
+    if (contains(subject, Events.suffix(event))) {
+      e = event;
     }
   });
   return e;
 }
 
 function expectedEvent(subject) {
-  var all = EVENTS.all();
+  var all = Events.all();
   var result = false;
-  all.forEach(function(event) {
-    result = result || contains(subject, event.suffix);
+  all.forEach(function (event) {
+    result = result || contains(subject, Events.suffix(event));
   });
   return result;
 }
