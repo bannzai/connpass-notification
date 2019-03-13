99f8d1c807783c8da24b05a83063db77de8bb7e3
bannzai <kingkong999yhirose@gmail.com>
2019-03-13 22:54:42 +0900

    :sparkles: Add extract event name

--- src/extractor.ts
+++ src/extractor.ts
@@ -31,3 +31,18 @@ export function extractContactSourceAddress(messageHTMLBody: string) {
   const regex = new RegExp(pattern);
   return messageHTMLBody.match(regex)[1]
 }
+
+/*
+  <a href="https://engineers-x-designers.connpass.com/event/120545/?utm_campaign=event_participate_to_owner&utm_source=notifications&utm_medium=email&utm_content=title_link" target="_blank" style="color:#000;"><strong>【増枠】DXEL.3 エンジニアとデザイナーが「いい関係」を築くために</strong></a>に参加登録しました。
+*/
+export function extractEventLink(messageHTMLBody: string) {
+  const pattern = "https://engineers-x-designers.connpass.com/event/[0-9]+/"
+  const regex = new RegExp(pattern);
+  return messageHTMLBody.match(regex)[0]
+}
+
+export function extractEventName(messageHTMLBody: string) {
+  const pattern = '<a href="https://engineers-x-designers.connpass.com/event/.+" target="_blank" style="color:#000;"><strong>(.+)</strong></a>に参加登録しました。'
+  const regex = new RegExp(pattern);
+  return messageHTMLBody.match(regex)[1]
+}
