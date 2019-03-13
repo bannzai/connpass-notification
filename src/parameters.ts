ff13d4750effec30117f002e5aba88537a74723f
bannzai <kingkong999yhirose@gmail.com>
2019-03-13 23:50:18 +0900

    :recycle: Remove unnecessary functions

--- src/parameters.ts
+++ src/parameters.ts
@@ -1,14 +1,6 @@
 import { Events } from './events'
 import { extractContactSourceAddress, extractContactSourceName, extractUserNameLink, extractUserName, extractEventName, extractEventLink, extractEventNameForContact, extractEventLinkForContact, extractEventNameForRegister, extractEventLinkForRegister, extractEventNameForCancel, extractEventLinkForCancel } from './extractor'
 
-function isContact(event: Events) {
-  return event == Events.Contact;
-}
-
-function isRegisterOrCancel(event: Events) {
-  return event == Events.Register || event == Events.Cancel;
-}
-
 export function buildFields(event: Events, messageHTMLBody: string) {
   switch (event) {
     case Events.Register:
