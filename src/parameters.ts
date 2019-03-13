3ce75d3e67bab39570358f1af522d50ecb25d0d3
bannzai <kingkong999yhirose@gmail.com>
2019-03-13 23:49:59 +0900

    :recycle: Switching with events

--- src/parameters.ts
+++ src/parameters.ts
@@ -1,5 +1,5 @@
 import { Events } from './events'
-import { extractContactSourceAddress, extractContactSourceName, extractUserNameLink, extractUserName, extractEventName, extractEventLink, extractEventNameForContact, extractEventLinkForContact } from './extractor'
+import { extractContactSourceAddress, extractContactSourceName, extractUserNameLink, extractUserName, extractEventName, extractEventLink, extractEventNameForContact, extractEventLinkForContact, extractEventNameForRegister, extractEventLinkForRegister, extractEventNameForCancel, extractEventLinkForCancel } from './extractor'
 
 function isContact(event: Events) {
   return event == Events.Contact;
@@ -10,36 +10,51 @@ function isRegisterOrCancel(event: Events) {
 }
 
 export function buildFields(event: Events, messageHTMLBody: string) {
-  if (isContact(event)) {
-    return [
-      {
-        "title": "Contact Source Address",
-        "value": extractContactSourceAddress(messageHTMLBody),
-      },
-      {
-        "title": "Contact Source Name",
-        "value": extractContactSourceName(messageHTMLBody)
-      },
-      {
-        "title": extractEventNameForContact(messageHTMLBody),
-        "value": extractEventLinkForContact(messageHTMLBody)
-      }
-    ]
-  }
-  if (isRegisterOrCancel(event)) {
-    return [
-      {
-        "title": "User Link",
-        "value": extractUserNameLink(messageHTMLBody),
-      },
-      {
-        "title": "User Name",
-        "value": extractUserName(messageHTMLBody)
-      },
-      {
-        "title": extractEventName(messageHTMLBody),
-        "value": extractEventLink(messageHTMLBody)
-      }
-    ]
+  switch (event) {
+    case Events.Register:
+      return [
+        {
+          "title": "User Link",
+          "value": extractUserNameLink(messageHTMLBody),
+        },
+        {
+          "title": "User Name",
+          "value": extractUserName(messageHTMLBody)
+        },
+        {
+          "title": extractEventNameForRegister(messageHTMLBody),
+          "value": extractEventLinkForRegister(messageHTMLBody)
+        }
+      ];
+    case Events.Cancel:
+      return [
+        {
+          "title": "User Link",
+          "value": extractUserNameLink(messageHTMLBody),
+        },
+        {
+          "title": "User Name",
+          "value": extractUserName(messageHTMLBody)
+        },
+        {
+          "title": extractEventNameForCancel(messageHTMLBody),
+          "value": extractEventLinkForCancel(messageHTMLBody)
+        }
+      ];
+    case Events.Contact:
+      return [
+        {
+          "title": "Contact Source Address",
+          "value": extractContactSourceAddress(messageHTMLBody),
+        },
+        {
+          "title": "Contact Source Name",
+          "value": extractContactSourceName(messageHTMLBody)
+        },
+        {
+          "title": extractEventNameForContact(messageHTMLBody),
+          "value": extractEventLinkForContact(messageHTMLBody)
+        }
+      ];
   }
 }
